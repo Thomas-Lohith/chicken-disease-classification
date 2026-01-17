9931823529552826ac1e705acbec243a9bad9b1f
Thomas-Lohith <thomas.routhu@gmail.com>
2026-01-17 20:32:11 +0100

    added data ingestion trial scripts

--- src/cnnClassifier/__init__.py
+++ src/cnnClassifier/__init__.py
@@ -9,7 +9,7 @@ log_filepath = os.path.join(log_dir, 'running_logs.log')
 os.makedirs(log_dir, exist_ok=True)
 
 
-logging.basicconfig(
+logging.basicConfig(
     level= logging.INFO,
     format = logging_str,
     handlers = [
